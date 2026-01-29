635347e6f80caf0f5b27ff9a115e52a2d6ae5146
Saalif Darial <saalifdarial@gmail.com>
2026-01-29 16:02:03 +0530

    return the square of a number

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
-// This is a comment
+#include <iostream>
+
+int main() {
+    using namespace std;
+
+    int Num;
+
+    cout << "Enter a Number: ";
+    cin >> Num;
+
+    cout << "The square of " << Num << " is " << Num * Num << endl;
+
+    return 0;
+
+}
